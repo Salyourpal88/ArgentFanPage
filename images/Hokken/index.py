968a68435942ae23a2f41b6a81f3890223c228b2
Salyourpal88 <99516661+Salyourpal88@users.noreply.github.com>
2023-09-09 10:18:07 -0700

    Fixed shading

--- images/Hokken/index.py
+++ images/Hokken/index.py
@@ -1,35 +1,38 @@
 from PIL import Image
 import os
- 
+
+
 def convertImage(filename):
     img = Image.open(filename)
     img = img.convert("RGBA")
- 
+
     datas = img.getdata()
-    
+
     wi, hi = 0, 0
     width, height = img.size
     newData = []
- 
+
+    x = datas.getpixel((30, 0))
+
     for item in datas:
         wi += 1
         if (wi < 25 or width - 25 < wi) and (hi < 20 or hi > height - 25):
-            if item[0] > 70 and item[1] > 70 and item[2] > 70: 
+            if item[3] == 0:
                 newData.append((255, 255, 255, 0))
             else:
-                newData.append((4, 8, 9, 255))
+                newData.append(x)
         else:
             newData.append(item)
-        if(wi == width):
+        if wi == width:
             wi = 0
             hi += 1
     img.putdata(newData)
     img.save(filename, "WEBP")
     print("Successful")
- 
-#
-directory ='./'
+
+
+directory = "./"
 
 for filename in os.listdir(directory):
-    if '.webp' in filename:
+    if ".webp" in filename:
         convertImage(filename)
