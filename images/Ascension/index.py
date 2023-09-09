b7fd5c81fd69a3279aff9e31df1ed38220dbbace
Salyourpal88 <99516661+Salyourpal88@users.noreply.github.com>
2023-09-09 09:08:13 -0700

    Fixed Borders

--- images/Ascension/index.py
+++ images/Ascension/index.py
@@ -1,35 +1,36 @@
 from PIL import Image
 import os
- 
+
+
 def convertImage(filename):
     img = Image.open(filename)
     img = img.convert("RGBA")
- 
+
     datas = img.getdata()
-    
+
     wi, hi = 0, 0
     width, height = img.size
     newData = []
- 
     for item in datas:
         wi += 1
-        if (wi < 25 or width - 25 < wi) and (hi < 20 or hi > height - 25):
-            if item[0] > 70 and item[1] > 70 and item[2] > 70: 
+        if (wi < 30 or width - 30 < wi) and (hi < 28 or hi > height - 30):
+            if item[0] > 70 and item[1] > 70 and item[2] > 70:
                 newData.append((255, 255, 255, 0))
             else:
-                newData.append((4, 8, 9, 255))
+                newData.append((1, 1, 1, 255))
         else:
             newData.append(item)
-        if(wi == width):
+        if wi == width:
             wi = 0
             hi += 1
     img.putdata(newData)
     img.save(filename, "WEBP")
     print("Successful")
- 
+
+
 #
-directory ='./'
+directory = "./"
 
 for filename in os.listdir(directory):
-    if '.webp' in filename:
+    if ".webp" in filename:
         convertImage(filename)
