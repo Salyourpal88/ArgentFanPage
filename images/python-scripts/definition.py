9b7ca44e49282df1327daa0fbed69fef7a39733a
Salyourpal88 <99516661+Salyourpal88@users.noreply.github.com>
2023-09-15 16:57:21 -0700

    small fixes

--- images/python-scripts/definition.py
+++ images/python-scripts/definition.py
@@ -1,35 +1,61 @@
 from PIL import Image
 import os
- 
+import numpy as np
+
+
 def convertImage(filename):
     img = Image.open(filename)
     img = img.convert("RGBA")
- 
+
     datas = img.getdata()
-    
-    wi, hi = 0, 0
     width, height = img.size
     newData = []
- 
-    for item in datas:
-        wi += 1
-        if (wi < 25 or width - 25 < wi) and (hi < 20 or hi > height - 25):
-            if item[0] > 70 and item[1] > 70 and item[2] > 70: 
-                newData.append((255, 255, 255, 0))
+    psycho = []
+    sal = []
+    for y in range(height):
+        for x in range(width):
+            pixel = datas.getpixel((x, y))
+            if y < 29 or x < 34 or x > 599 or y > 845:
+                if pixel[3] == 0:
+                    s = 0
+                else:
+                    sal.append(pixel)
+
+    for y in range(height):
+        for x in range(width):
+            pixel = datas.getpixel((x, y))
+            if y < 29 or x < 34 or x > 599 or y > 845:
+                if pixel in sal:
+                    newData.append((4, 6, 8, 255))
+                else:
+                    newData.append(pixel)
+            elif y >= 29 and x >= 34 and x < 50 and y < 41:
+                if pixel in sal:
+                    newData.append((4, 6, 8, 255))
+                else:
+                    newData.append(pixel)
+            elif y <= 855 and x >= 34 and x < 50 and y > 831:
+                if pixel in sal:
+                    newData.append((4, 6, 8, 255))
+                else:
+                    newData.append(pixel)
+            elif y <= 855 and y > 831 and x <= 599 and x > 585:
+                if pixel in sal:
+                    newData.append((4, 6, 8, 255))
+                else:
+                    newData.append(pixel)
+            elif y >= 29 and y < 41 and x <= 599 and x > 585:
+                if pixel in sal:
+                    newData.append((4, 6, 8, 255))
+                else:
+                    newData.append(pixel)
             else:
-                newData.append((4, 8, 9, 255))
-        else:
-            newData.append(item)
-        if(wi == width):
-            wi = 0
-            hi += 1
+                newData.append(pixel)
     img.putdata(newData)
     img.save(filename, "WEBP")
     print("Successful")
- 
-#
-directory ='./'
 
-for filename in os.listdir(directory):
-    if '.webp' in filename:
-        convertImage(filename)
+
+convertImage("../Intro-Deck-2019/hurricane-shot.webp")
+convertImage("../Intro-Deck-2019/tornado-shot.webp")
+convertImage("../Intro-Deck-2019/tower-of-air.webp")
