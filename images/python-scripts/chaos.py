716e67c6b4049a1cebfa0bf78b1c45ab1fabf525
Salyourpal88 <99516661+Salyourpal88@users.noreply.github.com>
2023-09-19 10:50:38 -0700

    submit change

--- images/python-scripts/chaos.py
+++ images/python-scripts/chaos.py
@@ -20,7 +20,8 @@ def convertImage(filename):
     # Crops the image and restablishes borders
     img = img.crop((ls, ts, rs, bs))
     img = img.convert("RGBA")
-    datas = img.getdata()
+    check = img.getdata()
+    datas = list(img.getdata())
     width, height = img.size
 
     tc = True
@@ -28,26 +29,77 @@ def convertImage(filename):
 
     for y in range(height):
         for x in range(width):
-            pixel = datas.getpixel((x, y))
-            if x <= 4 or x >= width - 7 or y <= 6 or y >= height - 7:
-                sal.append(pixel)
+            pixels = check.getpixel((x, y))
+            if pixels[0] >= 28 and pixels[1] >= 28 and pixels[2] >= 28 and tc:
+                pixel_list = list(datas[y * img.width + x])
 
-    for y in range(height):
-        for x in range(width):
-            pixel = datas.getpixel((x, y))
-            if (pixel in sal and tc) or x in range(width - 7, width):
-                if pixel[0] >= 28 and pixel[1] >= 28 and pixel[2] >= 28:
-                    newData.append((255, 255, 255, 0))
-                else:
-                    newData.append((0, 0, 0, 255))
+                # Change the index value of the pixel in the list.
+                pixel_list[0] = 255
+                pixel_list[1] = 255
+                pixel_list[2] = 255
+                pixel_list[3] = 0
+
+                # Convert the list pixel value back to a tuple.
+                datas[y * img.width + x] = tuple(pixel_list)
             else:
                 tc = False
-                newData.append(pixel)
         tc = True
 
-    img.putdata(newData)
+    for y in range(height):
+        for x in reversed(range(width)):
+            pixels = check.getpixel((x, y))
+            if pixels[0] >= 28 and pixels[1] >= 28 and pixels[2] >= 28 and tc:
+                pixel_list = list(datas[y * img.width + x])
+
+                # Change the index value of the pixel in the list.
+                pixel_list[0] = 255
+                pixel_list[1] = 255
+                pixel_list[2] = 255
+                pixel_list[3] = 0
+
+                # Convert the list pixel value back to a tuple.
+                datas[y * img.width + x] = tuple(pixel_list)
+            else:
+                tc = False
+        tc = True
+
+    img.putdata(datas)
+    filename = filename.replace("../Expansion-Pack-Generations/", "")
     img.save(filename, "WEBP")
     print("Success")
+
+    # for y in range(height):
+    #     for x in range(width):
+    #         pixel = datas.getpixel((x, y))
+    #         if x == 8 or x == width - 9 or y == 6 or y == height - 10:
+    #             newData.append((255, 255, 255, 255))
+    #         else:
+    #             newData.append(pixel)
+
+    # for y in range(height):
+    #     for x in range(width):
+    #         pixel = datas.getpixel((x, y))
+    #         if x <= 8 or x >= width - 9 or y <= 6 or y >= height - 10:
+    #             if pixel not in sal:
+    #                 sal.append(pixel)
+
+    # for y in range(height):
+    #     for x in range(width):
+    #         pixel = datas.getpixel((x, y))
+    #         if (pixel in sal and tc) or x in range(width - 7, width):
+    #             if pixel[0] >= 28 and pixel[1] >= 28 and pixel[2] >= 28:
+    #                 newData.append((255, 255, 255, 0))
+    #             else:
+    #                 newData.append((0, 0, 0, 255))
+    #         else:
+    #             tc = False
+    #             newData.append(pixel)
+    #     tc = True
+
+    # img.putdata(newData)
+    # filename = filename.replace("../Expansion-Pack-Generations/", "")
+    # img.save(filename, "WEBP")
+    # print("Success")
 
 
 def findLeftSide(datas, wi, hi):
@@ -89,8 +141,8 @@ def findBottomSide(datas, wi, hi):
 #         convertImage(directory + filename)
 
 # convertImage("../Expansion-Pack-Generations/corona-the-spirit-ruby.webp")
-# convertImage("../Expansion-Pack-Generations/198206.webp")
-# convertImage("../Expansion-Pack-Generations/198207.webp")
-# convertImage("../Expansion-Pack-Generations/198208.webp")
-convertImage("../Expansion-Pack-Generations/ergon-the-tragic-gargoyle.webp")
-# convertImage("../Expansion-Pack-Generations/198213.webp")
+convertImage("../Expansion-Pack-Generations/198206.webp")
+convertImage("../Expansion-Pack-Generations/198207.webp")
+convertImage("../Expansion-Pack-Generations/198208.webp")
+convertImage("../Expansion-Pack-Generations/198206.webp")
+convertImage("../Expansion-Pack-Generations/198213.webp")
