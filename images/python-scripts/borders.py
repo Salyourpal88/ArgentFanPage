f70cd78e24a3287c8f99af24a8e92b8b072999fe
Salyourpal88 <99516661+Salyourpal88@users.noreply.github.com>
2023-09-19 16:45:39 -0700

    fixed borders

---
+++ images/python-scripts/borders.py
@@ -0,0 +1,123 @@
+from PIL import Image
+import os
+import numpy as np
+
+
+def convertImage(filename):
+    # Gets the first image, then saves the data
+    img = Image.open(filename)
+    img = img.convert("RGBA")
+    width, height = img.size
+
+    check = img.getdata()
+    datas = list(img.getdata())
+    width, height = img.size
+    newData = []
+
+    tc = True
+    for y in range(height):
+        for x in range(width):
+            pixels = check.getpixel((x, y))
+            if pixels[0] >= 21 and pixels[1] >= 21 and pixels[2] >= 21 and tc:
+                pixel_list = list(datas[y * img.width + x])
+                # Change the index value of the pixel in the list.
+                pixel_list[0] = 255
+                pixel_list[1] = 255
+                pixel_list[2] = 255
+                pixel_list[3] = 0
+                # Convert the list pixel value back to a tuple.
+                datas[y * img.width + x] = tuple(pixel_list)
+            else:
+                tc = False
+        tc = True
+
+    for y in range(height):
+        for x in reversed(range(width)):
+            pixels = check.getpixel((x, y))
+            if pixels[0] >= 21 and pixels[1] >= 21 and pixels[2] >= 21 and tc:
+                pixel_list = list(datas[y * img.width + x])
+                # Change the index value of the pixel in the list.
+                pixel_list[0] = 255
+                pixel_list[1] = 255
+                pixel_list[2] = 255
+                pixel_list[3] = 0
+                # Convert the list pixel value back to a tuple.
+                datas[y * img.width + x] = tuple(pixel_list)
+            else:
+                tc = False
+        tc = True
+
+    img.putdata(datas)
+    img.save(filename, "WEBP")
+    print("Success")
+
+    # for y in range(height):
+    #     for x in range(width):
+    #         pixel = datas.getpixel((x, y))
+    #         if x == 8 or x == width - 9 or y == 6 or y == height - 10:
+    #             newData.append((255, 255, 255, 255))
+    #         else:
+    #             newData.append(pixel)
+
+    # for y in range(height):
+    #     for x in range(width):
+    #         pixel = datas.getpixel((x, y))
+    #         if x <= 8 or x >= width - 9 or y <= 6 or y >= height - 10:
+    #             if pixel not in sal:
+    #                 sal.append(pixel)
+
+    # for y in range(height):
+    #     for x in range(width):
+    #         pixel = datas.getpixel((x, y))
+    #         if (pixel in sal and tc) or x in range(width - 7, width):
+    #             if pixel[0] >= 139 and pixel[1] >= 139 and pixel[2] >= 139:
+    #                 newData.append((255, 255, 255, 0))
+    #             else:
+    #                 newData.append((0, 0, 0, 255))
+    #         else:
+    #             tc = False
+    #             newData.append(pixel)
+    #     tc = True
+
+    # img.putdata(newData)
+    # filename = filename.replace("../Expansion-Pack-Generations/", "")
+    # img.save(filename, "WEBP")
+    # print("Success")
+
+
+def findLeftSide(datas, wi, hi):
+    for x in range(wi):
+        for y in range(hi):
+            pixel = datas.getpixel((x, y))
+            if pixel[0] < 88 and pixel[1] < 88 and pixel[2] < 88:
+                return x
+
+
+def findRightSide(datas, wi, hi):
+    for x in reversed(range(wi)):
+        for y in range(hi):
+            pixel = datas.getpixel((x, y))
+            if pixel[0] < 88 and pixel[1] < 88 and pixel[2] < 88:
+                return x
+
+
+def findTopSide(datas, wi, hi):
+    for y in range(hi):
+        for x in range(wi):
+            pixel = datas.getpixel((x, y))
+            if pixel[0] < 88 and pixel[1] < 88 and pixel[2] < 88:
+                return y
+
+
+def findBottomSide(datas, wi, hi):
+    for y in reversed(range(hi)):
+        for x in range(wi):
+            pixel = datas.getpixel((x, y))
+            if pixel[0] < 88 and pixel[1] < 88 and pixel[2] < 88:
+                return y
+
+
+directory = "../Expansion-Pack-Conviction/"
+for filename in os.listdir(directory):
+    if ".webp" in filename:
+        convertImage(directory + filename)
